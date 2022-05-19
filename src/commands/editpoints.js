8537d65fe7632ea1c6ec411b642d212410bfe9cd
Matthew Marcuzzo <42529379+MattM123@users.noreply.github.com>
2022-05-18 22:30:43 -0500

    fixed inc maybe

--- src/commands/editpoints.js
+++ src/commands/editpoints.js
@@ -59,42 +59,23 @@ class EditPoints extends Command {
           })
         }
 
-      //amount can never be negative 
-      if (amount < 0) {
-        amount = amount * -1
-      }
-      
       if (options.getString('operation') == 'add' && message.member.roles.find(role => role.hasPermission('Administrator'))) {
         //increments users team points by provided integer or double
-        increment = pointsTotal + amount;
         await userData.updateOne(
-            { $inc: { pointsTotal: increment } },
+            { $inc: { pointsTotal: amount } },
             { upsert: true },
           ).lean()
       }
          
 
       else if (options.getString('operation') == 'subtract' && message.member.roles.find(role => role.hasPermission('Administrator'))) {
-        decrement = pointsTotal - amount;
-        //Negative case
-        if (decrement < 0) {
-          return i.reply({
-            embeds: [
-              new Discord.MessageEmbed().setDescription(
-               `Cannot edit a users score to be a negative value`,
-              ),
-            ],
-         })
-        }
-  
-        else {
-          //decrements users team points by provided integer or double
-          await userData.updateOne(
-              { $dec: { pointsTotal: decrement } },
-              { upsert: true },
-            ).lean()
-      }
+        //decrements users team points by provided integer or double
+        await userData.updateOne(
+            { $inc: { pointsTotal: -1 * (amount) } },
+            { upsert: true },
+          ).lean()
     }
+    
     //Author of command must have admin perms on team
     else {
       return i.reply({
