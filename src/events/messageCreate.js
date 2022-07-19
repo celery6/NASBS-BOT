15ff42b50107f9ef3008901c5bd5adb0e095bfc6
celery <soccercli12@gmail.com>
2022-07-19 14:34:21 -0400

    improve submission format coordinate regex and error msg

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -12,24 +12,25 @@ async function execute(client, msg) {
   if (msg.channel.id == guild.submitChannel) {
     // check for images
     if (msg.attachments.size === 0) {
-      return reject(client, msg, guild)
+      return reject(client, msg, guild, 'NO IMAGE FOUND')
     }
 
     // ensure there are at least 2 lines (# and coords)
     const lines = msg.content.split('\n')
     if (lines.length < 2) {
-      return reject(client, msg, guild)
+      return reject(client, msg, guild, '')
     }
 
-    const regexExp =
-      /^\s*[(]?[-+]?([1-8]+\d\.(\d+)?|90(\.0+))\xb0?,?\s+[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]+\d))\.(\d+))\xb0?\s*/
+    const coordsRegex =
+      /^(\s*[(]?[-+]?([1-8]+\d\.(\d+)?|90(\.0+))\xb0?,?\s+[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]+\d))\.(\d+))\xb0?\s*)|(\s*(\d{1,3})\s*(?:°|d|º| |g|o)\s*([0-6]?\d)\s*(?:'|m| |´|’|′))/
+
     let coords = false
     let count = false
 
     // check content of each line for either count (including range x-z) or coords
     lines.forEach((line) => {
       line = line.replace(/#/g, '')
-      if (regexExp.test(line) === true) {
+      if (coordsRegex.test(line) === true) {
         coords = true
       } else if (!isNaN(line) && Number.isInteger(Number(line))) {
         count = true
@@ -43,15 +44,17 @@ async function execute(client, msg) {
     })
 
     // reject submission if it doesn't include coords or a count
-    if (coords !== true || count !== true) {
-      reject(client, msg, guild)
+    if (coords !== true) {
+      reject(client, msg, guild, 'INVALID OR UNRECOGNIZED COORDINATES')
+    } else if (count !== true) {
+      reject(client, msg, guild, 'BUILD COUNT NOT PRESENT')
     }
   }
 }
 
-async function reject(client, msg, guild) {
+async function reject(client, msg, guild, reason) {
   const embed = new Discord.MessageEmbed()
-    .setTitle(`INCORRECT SUBMISSION FORMAT.`)
+    .setTitle(`INCORRECT SUBMISSION FORMAT: ${reason}`)
     .setDescription(
       `**[Correct format:](${guild.formattingMsg})**\n[Build count]\n[Coordinates]\n[Location name] (OPTIONAL)\n[Image(s) of build]\n\n__The entire submission must be in ONE MESSAGE!__\nView [pinned message](${guild.formattingMsg}) for more details.`,
     )
