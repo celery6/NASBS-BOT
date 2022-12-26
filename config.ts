ee47c4ebe0bfaf9b455288fa3ff535f55e6c4fd8
celery <71736183+celery6@users.noreply.github.com>
2022-12-26 16:45:50 -0500

    gitignore

--- config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  token: 'NzE4NjkxMDA2MzI4OTk1ODkw.GY34mg.HSFYbaHPNLimGA_ZRj_mcxS06K26p3Al4Uo-tk',
-  clientId: '718691006328995890',
-  mongoURI: 'mongodb://bot:6;)%5Bf8rGG2xq6ZGg@207.244.229.33:27017/nasbs?authSource=admin',
-  test: true,
-}
-
-export default config
