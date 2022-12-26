ad75c1ea0e69b3f5d426261f43699e19ae688eee
celery <71736183+celery6@users.noreply.github.com>
2022-12-26 16:40:56 -0500

    update gitignore

--- config.ts
+++ config.ts
@@ -1,8 +1,7 @@
 const config = {
-  token: 'NzE4NjkxMDA2MzI4OTk1ODkw.XtsjKA.8j8_iiBubYzV0NJNykaR-ZOEInQ',
+  token: 'NzE4NjkxMDA2MzI4OTk1ODkw.GY34mg.HSFYbaHPNLimGA_ZRj_mcxS06K26p3Al4Uo-tk',
   clientId: '718691006328995890',
-  mongoURI:
-    'mongodb://bot:6;)%5Bf8rGG2xq6ZGg@207.244.229.33:27017/nasbs?authSource=admin',
+  mongoURI: 'mongodb://bot:6;)%5Bf8rGG2xq6ZGg@207.244.229.33:27017/nasbs?authSource=admin',
   test: true,
 }
 
